b0af266f6bec5db104dd01e6057d1ba565a29e20
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-03-07 12:57:32 +0100

    Integration des Migrations && Bases de donnés.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,22 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+use Database\Seeders\Groupes;
+use Database\Seeders\Studetns_Group;
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  public function run (){
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+ $this->call(Studetns_Group::class);
+//  $this->call(Groupes::class);
+
+
+  }
 }
